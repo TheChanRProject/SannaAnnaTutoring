b95a023dfab6101feadcec0c0340ca6f3940c66e
quantumLTraining <garpchoo1@gmail.com>
2018-04-12 13:27:32 -0700

    adding txt files

---
+++ math/functionNine.js
@@ -0,0 +1,10 @@
+function pOfX(x) {
+  theFunction = 2*Math.abs(x+1) - 3
+  console.log(theFunction);
+}
+pOfX(1.5)
+function qOfX(x) {
+  theFunction = (-x^2 + 16) / 7
+  console.log(theFunction);
+}
+qOfX(1.5)
