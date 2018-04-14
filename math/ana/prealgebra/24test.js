7b3172216412df7db8932c774cf13b5aacb7064f
quantumLTraining <garpchoo1@gmail.com>
2018-04-14 13:04:18 -0700

    adding new txt file and updating 24test.js

--- math/ana/prealgebra/24test.js
+++ math/ana/prealgebra/24test.js
@@ -1,43 +1,49 @@
-function interceptedArc(x) {
-  console.log(2*x);
-}
-interceptedArc(36)
+// function interceptedArc(x) {
+//   console.log(2*x);
+// }
+// interceptedArc(36)
+//
+//
+// function triangleInequality(a,b,c) {
+//   pyArgs = a**2 + b**2
+//   if (pyArgs == c**2) {
+//     console.log("True");
+//   }
+//   else {
+//     console.log("False");
+//   }
+// }
+// triangleInequality(6,8,11)
+// triangleInequality(10,24,25)
+// triangleInequality(7,24,26)
+// triangleInequality(8,15,17)
+//
+//
+//
+//
+// function interiorSum(n) {
+//   formula = (n-2)*180
+//   console.log(formula);
+// }
+// interiorSum(6)
+//
+// function interiorAngle(n) {
+//   formula = ((n-2)*180)/n
+//   console.log(formula);
+// }
+// interiorAngle(9)
+// interiorAngle(7)
+// interiorAngle(8)
+// interiorAngle(10)
+//
+// const c = Math.sqrt(Math.pow(30,2) + Math.pow(20, 2))
+// console.log(c);
+//
+// const x = 182 / 8
+// console.log(2*x + 7);
 
+// 4x = -5(x-18)
+// 4x = -5x + 90
 
-function triangleInequality(a,b,c) {
-  pyArgs = a**2 + b**2
-  if (pyArgs == c**2) {
-    console.log("True");
-  }
-  else {
-    console.log("False");
-  }
-}
-triangleInequality(6,8,11)
-triangleInequality(10,24,25)
-triangleInequality(7,24,26)
-triangleInequality(8,15,17)
-
-
-
-
-function interiorSum(n) {
-  formula = (n-2)*180
-  console.log(formula);
-}
-interiorSum(6)
-
-function interiorAngle(n) {
-  formula = ((n-2)*180)/n
-  console.log(formula);
-}
-interiorAngle(9)
-interiorAngle(7)
-interiorAngle(8)
-interiorAngle(10)
-
-const c = Math.sqrt(Math.pow(30,2) + Math.pow(20, 2))
-console.log(c);
-
-const x = 182 / 8
-console.log(2*x + 7);
+const x2 = 90 / 9
+console.log(4*x2);
