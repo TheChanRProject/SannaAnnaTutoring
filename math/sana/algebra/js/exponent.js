c2cef8a172a7abf0918a24b3b5fea4d8249ffd6a
quantumLTraining <garpchoo1@gmail.com>
2018-04-14 13:59:40 -0700

    adding js file

---
+++ math/sana/algebra/js/exponent.js
@@ -0,0 +1,57 @@
+const answer = 3 * Math.pow(6,0)
+console.log(answer);
+
+const answer2 = Math.pow(3, -1) + Math.pow(2, -1)
+console.log(answer2);
+console.log(5/6);
+
+const answer3 = 1 / (Math.pow(3, -2))
+console.log(answer3);
+
+const answer4 = Math.pow(0.5, -2)
+console.log(answer4);
+
+const answer7 = Math.pow(16, 0.5)
+console.log(answer7);
+
+const answer8 = Math.pow(81, 0.75)
+console.log(answer8);
+
+const answer9 = Math.pow(-32, 0.6)
+console.log(answer9);
+
+const answer11 = Math.pow(27, -0.666)
+console.log(answer11);
+console.log(1/9);
+
+const answer12 = Math.pow(3, -2)
+console.log(answer12);
+
+const answer22 = Math.pow(9, 0.5)
+console.log(answer22);
+
+const answer23 = Math.log2(64)
+console.log(answer23);
+
+const answer24 = Math.log2(-32)
+console.log(answer24);
+
+function getBaseLog(x, y) {
+  console.log(Math.log(y) / Math.log(x));
+  return Math.log(y) / Math.log(x);
+
+}
+getBaseLog(9, 27)
+getBaseLog(3,27)
+
+function inGraph(x,y) {
+  if (y == Math.pow(3, 2*x)) {
+    console.log("true");
+  }
+  else {
+    console.log("false");
+  }
+}
+inGraph(-1,-9)
+inGraph(1,9)
+inGraph(0.5, 3)
