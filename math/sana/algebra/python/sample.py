eaa092d6410a558d6f544126f095775bd9dd6684
Rishov Chatterjee <garpchoo@gmail.com>
2018-04-17 19:16:16 -0700

    adding virtual environment and fixed matplotlib issue

--- math/sana/algebra/python/sample.py
+++ math/sana/algebra/python/sample.py
@@ -1,2 +1,17 @@
 import numpy as np
+import matplotlib as mpl
+mpl.use('TkAgg')
 import matplotlib.pyplot as plt
+
+xVals = np.arange(-10,10    ,0.1)
+
+def polynomial1(x):
+    return pow(3*x,2) + 4*x + 2
+
+yVals = polynomial1(xVals)
+
+plt.plot(xVals, yVals)
+plt.title("Sample Quadratic Function")
+plt.xlabel("X Values")
+plt.ylabel("Y Values")
+plt.show()
