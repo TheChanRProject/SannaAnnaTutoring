fb06b68693f5610f2f86b11000548fdb8c426d8d
Rishov Chatterjee <garpchoo@gmail.com>
2019-01-17 22:09:41 -0800

    editing

---
+++ science/sanna/physics/scripts/motion_graphs_project.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+x = np.array([float(i) for i in range(0,11)])
+print(x)
+
+y = np.array([0.00, 2.25, 4.38, 6.30, 8.30, 10.26, 12.30, 14.22, 16.43, 18.35, 20.00])
+print(y)
+
+import matplotlib.pyplot as plt
+plt.plot(x,y, 'b-')
+plt.savefig('motion-graph.png') 
