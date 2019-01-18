b13106d87d3b0524ff447a135c34a7c953b09c76
Rishov Chatterjee <garpchoo@gmail.com>
2019-01-17 22:11:17 -0800

    editing

--- science/sanna/physics/scripts/motion_graphs_project.py
+++ science/sanna/physics/scripts/motion_graphs_project.py
@@ -8,5 +8,8 @@ y = np.array([0.00, 2.25, 4.38, 6.30, 8.30, 10.26, 12.30, 14.22, 16.43, 18.35, 2
 print(y)
 
 import matplotlib.pyplot as plt
-plt.plot(x,y, 'b-')
-plt.savefig('motion-graph.png') 
+plt.title("Motion Graph")
+plt.xlabel("Distance (meters)")
+plt.ylabel("Time (seconds)")
+plt.plot(x,y, 'b-', marker='o')
+plt.savefig('motion-graph.png')
