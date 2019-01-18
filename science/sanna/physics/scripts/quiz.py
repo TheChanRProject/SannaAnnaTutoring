5b4ce14687c275caf43b373e7fab6930a9653698
Rishov Chatterjee <garpchoo@gmail.com>
2019-01-17 22:24:14 -0800

    done

--- science/sanna/physics/scripts/quiz.py
+++ science/sanna/physics/scripts/quiz.py
@@ -2,3 +2,9 @@ import numpy as np
 
 speed_1 = 520/12
 print(speed_1)
+
+from IPython.core.display import display, Latex
+
+display(Latex("$$ s = \\frac{d}{t} \n d = st \n d = 52 * 1.5 $$"))
+d_1 = 52 * 1.5
+print(d_1)
