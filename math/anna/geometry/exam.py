4c3a628313f1c649e0e2d2d9d0f7bf04427773b0
TheChanRProject <garpchoo@gmail.com>
2019-07-27 18:50:29 -0700

    Making

--- math/anna/geometry/exam.py
+++ math/anna/geometry/exam.py
@@ -1,1 +1,13 @@
 import numpy as np
+
+class Circle():
+    def __init__(self, r):
+        self.radius = r
+    def circumference(self):
+        return 2*np.pi*self.radius
+    def area(self):
+        return np.pi * np.power(self.radius,2)
+
+circ = Circle(r = 4)
+circ.circumference()
+circ.area()
