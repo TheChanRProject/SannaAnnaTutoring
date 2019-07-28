cf360920e16e7c71683cda08866ce5c4850a7d9a
TheChanRProject <garpchoo@gmail.com>
2019-07-27 21:23:43 -0700

    Making

--- math/anna/geometry/polygons/assignment_2/a2.py
+++ math/anna/geometry/polygons/assignment_2/a2.py
@@ -2,9 +2,19 @@ import numpy
 
 class Rectangle():
     def __init__(self, b, h):
-        self.base = b
-        self.height = h
-    def perimeter(self):
-        return 2*self.base + 2*self.height
-    def area(self):
-        return self.base * self.height
+        self.base_feet = b
+        self.height_feet = h
+        self.base_inches = b * 12
+        self.height_inches = b * 12
+    def feet_perimeter(self):
+        return 2*self.base_feet + 2*self.height_feet
+    def feet_area(self):
+        return self.base_feet * self.height_feet
+    def inches_area(self):
+        return self.base_inches * self.height_inches
+    def inches_perimeter(self):
+        return 2*self.base_inches * self.height_inches
+
+
+rec1 = Rectangle(18, 2)
+rec1.feet_area()
