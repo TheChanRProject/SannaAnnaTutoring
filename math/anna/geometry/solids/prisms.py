98b5f4de028bf1d1edcf8ef692b24f5927dc5460
TheChanRProject <garpchoo@gmail.com>
2019-08-04 17:31:17 -0700

    Creating edits

--- math/anna/geometry/solids/prisms.py
+++ math/anna/geometry/solids/prisms.py
@@ -1,9 +1,17 @@
 def surface_area(l,w1, w2, lateral_h, base_h, base_shape: str):
-    lateral_area = (l+w1+w2)*lateral_h
     if base_shape == "triangle":
+        lateral_area = (l+w1+w2)*lateral_h
         two_b = 2*(0.5)*l*base_h
     if base_shape == "rectangle":
+        lateral_area = (2*l + w1 + w2)*lateral_h
         two_b = 2*l*base_h
     return lateral_area + two_b
 
 print(surface_area(14, 13, 15, 20, 12, 'triangle'))
+
+print(surface_area(5, 2, 2, 3, 2, "rectangle"))
+96 * 96**0.5
+96 /6
+24 * 12
+18 * *
+18 * 8
