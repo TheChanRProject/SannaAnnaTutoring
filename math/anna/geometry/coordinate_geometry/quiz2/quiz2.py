e17ccd5ed81c33024cd3644c7a0de9425db019c1
TheChanRProject <garpchoo@gmail.com>
2019-08-23 19:11:30 -0700

    Creating edits

--- math/anna/geometry/coordinate_geometry/quiz2/quiz2.py
+++ math/anna/geometry/coordinate_geometry/quiz2/quiz2.py
@@ -38,3 +38,22 @@ plt.figure(figsize=(10,10))
 plt.plot(X,Y, 'r-')
 plt.title("Plot of Rectangular Garden", color="silver", size="24")
 plt.savefig("math/anna/geometry/coordinate_geometry/quiz2/q34/rectangular_garden.png")
+
+
+def q36_circle(x, y, r):
+    circle = (x - 3)**2 + y**2
+    if circle < r**2:
+        print("Interior")
+    elif circle == r**2:
+        print("On the circle")
+    elif circle > r**2:
+        print("Exterior")
+    return circle
+
+A = (-1, 1)
+B = (10, 0)
+C = (4, -8)
+
+q36_circle(A[0], A[1], 7)
+q36_circle(B[0], B[1], 7)
+q36_circle(C[0], C[1], 7)
