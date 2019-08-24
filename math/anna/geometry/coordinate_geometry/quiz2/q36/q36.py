bdf9d53b99c71db7e647fe90d67665062dba3f02
TheChanRProject <garpchoo@gmail.com>
2019-08-23 19:28:17 -0700

    Creating edits

--- math/anna/geometry/coordinate_geometry/quiz2/q36/q36.py
+++ math/anna/geometry/coordinate_geometry/quiz2/q36/q36.py
@@ -15,3 +15,19 @@ C = (4, -8)
 q36_circle(A[0], A[1], 7)
 q36_circle(B[0], B[1], 7)
 q36_circle(C[0], C[1], 7)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from jupyterthemes import jtplot
+jtplot.style(theme="monokai", grid=True)
+
+X = np.linspace(-10, 10, 100)
+Y = np.linspace(-10, 10, 100)
+X, Y = np.meshgrid(X,Y)
+F = (X - 3)**2 + (Y)**2 - 49
+plt.figure(figsize=(10,10))
+plt.contour(X,Y,F,[0], colors=['purple'])
+x,y = np.array([[-1, 1], [10, 0], [4, -8]]).T
+plt.scatter(x,y, color="blue")
+plt.title(r"Graph of $(x-3)^2 + y^2 = 49$", size=24, color="silver")
+plt.savefig("math/anna/geometry/coordinate_geometry/quiz2/q36/circle.png")
