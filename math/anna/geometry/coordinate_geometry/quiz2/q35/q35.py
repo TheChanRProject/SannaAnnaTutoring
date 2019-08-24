174b42eb86dd3c1bf13336a2f361393d26c6b0c7
TheChanRProject <garpchoo@gmail.com>
2019-08-23 19:02:55 -0700

    Creating edits

--- math/anna/geometry/coordinate_geometry/quiz2/q35/q35.py
+++ math/anna/geometry/coordinate_geometry/quiz2/q35/q35.py
@@ -1,2 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+from jupyterthemes import jtplot
+jtplot.style(theme="monokai", grid=False)
+plt.figure(figsize=(10,10))
+plt.axes()
+circle = Circle((3, (-7/2)), radius=7/2, fc='b')
+plt.gca().add_patch(circle)
+plt.axis('scaled')
+plt.savefig("math/anna/geometry/coordinate_geometry/quiz2/q35/circle.png")
+plt.show()
