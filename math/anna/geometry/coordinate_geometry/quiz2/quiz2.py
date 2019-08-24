b30a6ec552f98baf6eb95dfd73dd496402391e30
TheChanRProject <garpchoo@gmail.com>
2019-08-23 18:29:28 -0700

    Creating edits

--- math/anna/geometry/coordinate_geometry/quiz2/quiz2.py
+++ math/anna/geometry/coordinate_geometry/quiz2/quiz2.py
@@ -12,8 +12,8 @@ Y = [3, -4, -1, 3]
 plt.figure(figsize=(10,10))
 plt.plot(X,Y, 'g-')
 
-X = [2, 8, 8, 2]
-Y = [5, 5, 3, 3]
+X = [2, 8, 8, 2, 2]
+Y = [5, 5, 3, 3, 5]
 
 def distance(a, b):
     return np.sqrt(np.power(b[0] - a[0], 2) + np.power(b[1] - a[1], 2))
@@ -24,3 +24,17 @@ d3 = distance((8,3), (2,3))
 d4 = distance((2,5), (2,3))
 
 print(sum([d1, d2, d3 ,d4]))
+
+distances = [d1, d2, d3, d4]
+print(distances)
+area = distances[0] * distances[1]
+print(area)
+mulch = 6
+bags = area / mulch
+cost = 5.25 * bags
+print(cost)
+
+plt.figure(figsize=(10,10))
+plt.plot(X,Y, 'r-')
+plt.title("Plot of Rectangular Garden", color="silver", size="24")
+plt.savefig("math/anna/geometry/coordinate_geometry/quiz2/q34/rectangular_garden.png")
