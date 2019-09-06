95fa99143365cc6bbef39c4bc30bb52e3fe043b3
TheChanRProject <garpchoo@gmail.com>
2019-09-05 22:43:40 -0700

    Creating edits

--- math/anna/geometry/transformations/dilations_q19/d_q19.py
+++ math/anna/geometry/transformations/dilations_q19/d_q19.py
@@ -9,7 +9,7 @@ image_x = [-5, 2.5, 2.5, -5]
 image_y = [5, 5, -2.5, 5]
 
 line_x = np.linspace(-6, 3)
-line_y =
+line_y = -1*line_x
 plt.figure(figsize=(20,15))
 plt.plot(pre_image_x, pre_image_y, 'b-')
 plt.text(-2, 2, "A (-2, 2)", size=14)
@@ -19,3 +19,5 @@ plt.plot(image_x, image_y, 'g-')
 plt.text(-5, 5, "A' (-5, 5)", size=14)
 plt.text(2.5, 5, r"B' ($\frac{5}{2}$, 5)", size=14)
 plt.text(2.5, -2.5, r"C' ($\frac{5}{2}$, $-\frac{5}{2}$)", size=14)
+plt.plot(line_x, line_y, 'r-')
+plt.savefig("math/anna/geometry/transformations/dilations_q19/dilation.png")
