6f4ce3c303aafd701b94c7910c5faaf12cbb5cc9
TheChanRProject <garpchoo@gmail.com>
2019-09-10 21:54:23 -0700

    Creating edits

--- math/anna/geometry/trig/trig_ratios.py
+++ math/anna/geometry/trig/trig_ratios.py
@@ -13,3 +13,18 @@ np.sqrt(21)
 round(np.tan(28), 4)
 
 round(8 * np.cos(30), 4)
+
+np.tan(45)
+
+np.sin(30)
+
+np.tan(45) / np.sin(30)
+
+5 / np.sin(20)
+5 / (np.sin(20))
+np.sin(20)
+
+def degree_to_rad(deg_val):
+    return (deg_val * 2*np.pi) / 360
+
+5 / np.sin(degree_to_rad(20))
