a778986a5c7e3ec5273bbbddf032bdbfdfe3b58d
TheChanRProject <garpchoo@gmail.com>
2019-09-10 21:44:21 -0700

    Creating edits

---
+++ math/anna/geometry/trig/trig_ratios.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def pythag(a, b):
+    return np.sqrt(a**2 + b**2)
+
+pythag(7, 24)
+
+np.sqrt(29**2 - 21**2)
+
+pythag(4, np.sqrt(5))
+np.sqrt(21)
+
+round(np.tan(28), 4)
+
+round(8 * np.cos(30), 4)
