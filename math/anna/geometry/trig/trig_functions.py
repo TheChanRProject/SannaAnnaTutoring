1faed6e065186b73d4b6cc46937f8beb1f071a0a
TheChanRProject <garpchoo@gmail.com>
2019-09-11 22:50:15 -0700

    Creating edits

--- math/anna/geometry/trig/trig_functions.py
+++ math/anna/geometry/trig/trig_functions.py
@@ -1,8 +1,47 @@
 import numpy as np
 
-np.sin(np.deg2rad(30))
-np.tan(np.deg2rad(45)) / np.sin(np.deg2rad(30))
+np.arcsin(0.25)
+np.arcsin(np.deg2rad(0.25))
+np.rad2deg(np.arcsin(0.25))
+180 - (21 + 90)
+180 - (47 + 90)
+69 - 43
+(2500*np.sin(np.deg2rad(21)))/np.sin(np.deg2rad(26))
 
-15 * np.sin(np.deg2rad(20))  / (np.sin(np.deg2rad(55)))
+(2500 * np.tan(np.deg2rad(21))) / (np.tan(np.deg2rad(47)) - np.tan(np.deg2rad(21)))
+1394 * np.tan(np.deg2rad(47))
+180 - (33 + 63)
+(200*np.sin(np.deg2rad(63))) / np.sin(np.deg2rad(84))
+(40*np.sin(np.deg2rad(63))) / np.sin(np.deg2rad(12))
 
-(17 * np.sin(np.deg2rad(46))) / np.sin(np.deg2rad(31))
+np.rad2deg(np.arcsin(210/328))
+
+np.rad2deg(np.arcsin((84*np.sin(np.deg2rad(74))) / 126))
+400*np.sin(np.deg2rad(75))
+
+180 - (90 + 49)
+180 - (75 + 90)
+np.rad2deg(np.arcsin(1/3))
+225 + 81
+306 / 17
+
+(np.tan(np.deg2rad(60))) / np.cos(np.deg2rad(45))
+1 / np.sqrt(6)
+np.sqrt(6)
+600 * np.sin(np.deg2rad(75))
+np.rad2deg(np.arcsin(18 / 22))
+np.rad2deg(np.arccos(50 / 160))
+150 * np.sin(np.deg2rad(37))
+180 - (18 + 90)
+180 - (56 + 90)
+72 - 34
+(2200*np.sin(np.deg2rad(18))) / np.sin(np.deg2rad(38))
+1104*np.sin(np.deg2rad(56))
+180 - 108
+(9*np.sin(np.deg2rad(54))) / np.sin(np.deg2rad(72))
+180 - (65 + 80)
+(45*np.sin(np.deg2rad(65)))/np.sin(np.deg2rad(35))
+(10*np.sin(np.deg2rad(30))) / 4
+np.rad2deg(np.arcsin(((5*np.sin(np.deg2rad(45))) / 4)))
+np.rad2deg(np.arcsin((10*np.sin(np.deg2rad(85))) /13))
+np.rad2deg(np.arcsin((4*np.sin(np.deg2rad(150)))/7))
