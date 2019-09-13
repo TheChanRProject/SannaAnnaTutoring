8a77e97f215d6a53c9b42bd97173c674cd807267
TheChanRProject <garpchoo@gmail.com>
2019-09-12 20:33:51 -0700

    Creating edits

---
+++ math/anna/geometry/trig/law_of_cosines/law_of_cosines.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Cosine():
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+
+    def side(self,angle):
+        return np.sqrt(self.a**2 + self.b**2 - 2*self.a*self.b*np.cos(np.deg2rad(angle)))
+
+    def side_squared(self,angle):
+        return self.a**2 + self.b**2 - 2*self.a*self.b*np.cos(np.deg2rad(angle))
+
+    def angle(self,c):
+        argument = (c**2 - (self.a**2 + self.b**2)) / (2*self.a*self.b)
+        return np.rad2deg(np.arccos(argument))
