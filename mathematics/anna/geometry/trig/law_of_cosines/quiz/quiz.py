8640d32dbd92cb9ad47cce6fb325c04992ce2e30
TheChanRProject <garpchoo@gmail.com>
2019-09-12 21:59:05 -0700

    Creating edits

---
+++ mathematics/anna/geometry/trig/law_of_cosines/quiz/quiz.py
@@ -0,0 +1,16 @@
+from mathematics.anna.geometry.trig.law_of_cosines.law_of_cosines import Cosine
+
+prob1 = Cosine(10, 15)
+prob1.side(120)
+
+prob2 = Cosine(10, 12)
+prob2.side(24)
+
+prob3 = Cosine(7, 5)
+180 - prob3.angle(3)
+
+prob4 = Cosine(250, 300)
+prob4.angle(420)
+
+prob5 = Cosine(8,8)
+180 - prob5.angle(3)
