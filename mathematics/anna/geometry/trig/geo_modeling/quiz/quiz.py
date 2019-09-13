98d39045dc2a518d0c6dd5730f7730747f59d245
TheChanRProject <garpchoo@gmail.com>
2019-09-12 23:45:48 -0700

    Creating edits

--- mathematics/anna/geometry/trig/geo_modeling/quiz/quiz.py
+++ mathematics/anna/geometry/trig/geo_modeling/quiz/quiz.py
@@ -1,7 +1,21 @@
 import numpy as np
-
+from decimal import Decimal
 New_York = 8336697 / 302.64
 London = 3792621 / 469
 
 New_York
 London
+
+
+US = 9000000 / 313000000
+New_York = 727100 / 8336697
+
+US
+New_York
+
+
+
+'%.2E' % Decimal((5.9e17) / (1e2) )
+
+V = 30 * 45  * 75
+V
