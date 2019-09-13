68441d6ec230518e80b15e4ce46b4b8e0101edda
TheChanRProject <garpchoo@gmail.com>
2019-09-12 19:57:19 -0700

    Creating edits

--- math/anna/geometry/trig/law_of_cosines/q1/q1.py
+++ math/anna/geometry/trig/law_of_cosines/q1/q1.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 def law_of_cosines(a,b, angle):
-    return np.sqrt((a**2 + b**2 + a*b*np.cos(np.deg2rad(angle))))
+    return np.sqrt((a**2 + b**2 + 2*a*b*np.cos(np.deg2rad(angle))))
 
 print(law_of_cosines(16, 11, 33))
 
 16 * 11
+2 * 172
