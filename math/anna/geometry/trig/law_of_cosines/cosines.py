f6ff157105944ac1f2d939fa6c8fb74784b1e9f5
TheChanRProject <garpchoo@gmail.com>
2019-09-12 20:11:14 -0700

    Creating edits

--- math/anna/geometry/trig/law_of_cosines/cosines.py
+++ math/anna/geometry/trig/law_of_cosines/cosines.py
@@ -1,10 +1,13 @@
 import numpy as np
 def law_of_cosines(a,b, angle):
-    return np.sqrt(a**2 + b**2 + 2*a*b*np.cos(np.deg2rad(angle)))
+    return np.sqrt(a**2 + b**2 - 2*a*b*np.cos(np.deg2rad(angle)))
 
 def side_squared(a,b,angle):
-    return a**2 + b**2 + 2*a*b*np.cos(np.deg2rad(angle))
+    return a**2 + b**2 - 2*a*b*np.cos(np.deg2rad(angle))
 
 print(law_of_cosines(9, 5, 120))
 print(law_of_cosines(4, 7, 20))
-a
+print(side_squared())
+np.sqrt(16 + 49 + 2*(4*7*np.cos(np.deg2rad(20))))
+
+16 + (3.5)**2
