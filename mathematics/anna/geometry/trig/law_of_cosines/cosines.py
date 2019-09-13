224f7ccf424dbb4bb48fcfabd784154b69526e28
TheChanRProject <garpchoo@gmail.com>
2019-09-12 21:17:36 -0700

    Creating edits

--- mathematics/anna/geometry/trig/law_of_cosines/cosines.py
+++ mathematics/anna/geometry/trig/law_of_cosines/cosines.py
@@ -1,8 +1,31 @@
 from mathematics.anna.geometry.trig.law_of_cosines.law_of_cosines import Cosine
-
+import numpy as np
 prob1 = Cosine(9, 12)
 
 prob1.angle(16)
 
 256 - (81 + 144)
 18 * 12
+
+prob2 = Cosine(3,7)
+prob2.angle(5)
+
+prob5 = Cosine(4,5)
+prob5.angle(3)
+prob5 = Cosine(3,5)
+prob5.angle(4)
+prob5 = Cosine(3,4)
+prob5.angle(5)
+180 - 143
+
+prob6 = Cosine(2,2)
+prob6.side(90)
+
+prob7 = Cosine(5, 5)
+prob7.angle(7)
+
+prob8 = Cosine(8,6)
+prob8.side(60)
+
+prob8 = Cosine(7.2, 6)
+prob8.angle(8)
