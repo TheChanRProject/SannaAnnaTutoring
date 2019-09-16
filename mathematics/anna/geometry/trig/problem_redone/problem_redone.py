717877bd116878f10df248d690e019b3efa19bc8
TheChanRProject <garpchoo@gmail.com>
2019-09-16 14:05:49 -0700

    Creating edits

--- mathematics/anna/geometry/trig/problem_redone/problem_redone.py
+++ mathematics/anna/geometry/trig/problem_redone/problem_redone.py
@@ -1,1 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from jupyterthemes import jtplot
+jtplot.style(theme="monokai", grid=False)
+
+X = [0, 185, 185/2, 0]
+Y = [0, 0, 283, 0]
+plt.figure(figsize=(10,10))
+plt.plot(X,Y, 'g-')
+plt.text(185/2, 1, '185')
+plt.text(140, 283/2, '283')
+plt.text(170,3, r'$80^{\circ}$')
+plt.savefig("mathematics/anna/geometry/trig/problem_redone/triangle.png")
