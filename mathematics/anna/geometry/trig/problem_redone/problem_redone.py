d36c52fa1a3e367c6c8f0c9953e7e1200d44f07a
TheChanRProject <garpchoo@gmail.com>
2019-09-16 14:14:11 -0700

    Creating edits

--- mathematics/anna/geometry/trig/problem_redone/problem_redone.py
+++ mathematics/anna/geometry/trig/problem_redone/problem_redone.py
@@ -11,3 +11,8 @@ plt.text(185/2, 1, '185')
 plt.text(140, 283/2, '283')
 plt.text(170,3, r'$80^{\circ}$')
 plt.savefig("mathematics/anna/geometry/trig/problem_redone/triangle.png")
+
+ab = 185 * 283
+
+A = ab * np.sin(np.deg2rad(80))
+A
